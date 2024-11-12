fa38ca15ec170054c4cab2bed9712de5072f18c6
minospace <119105620+minospace@users.noreply.github.com>
2024-11-12 19:53:08 +0100

    added graphics file

---
+++ graphics.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  TG
+//
+//  Created by miro on 10/11/2024.
+//
+
+import Foundation
+
+struct graphics{
+    func character(){
+        print("""
+           -----
+          /     \\
+         /       --
+         \\     -
+           -----
+            |||\\
+            ||| \\
+            ||| ||
+            |||
+            |||
+           // \\
+          //   \\
+          ||    ||
+          ||__  ||__
+        """)
+    }
+    func flesh(){
+        print("""
+          /\\|\\
+         /////
+        /////
+       /\\/|
+    """)
+        print("flesh")
+    }
+}
